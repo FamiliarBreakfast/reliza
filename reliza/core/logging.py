47df7f29a2c001cdd3d1ed53bd59c4fed41064f1
FamiliarBreakfast <48958412+FamiliarBreakfast@users.noreply.github.com>
2022-06-10 13:44:16 -0400

    logging to both stdout and file

--- reliza/core/logging.py
+++ reliza/core/logging.py
@@ -1,16 +1,15 @@
 import os
 import time
+import sys
 import logging
 
 # check if log directory exists
 if not os.path.exists('logs'):
-    os.makedirs('logs')
+	os.makedirs('logs')
 
 logging.basicConfig(level=logging.INFO,
-                    format='[%(asctime)s] %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename=f'logs/{time.strftime("%Y-%m-%d_%H-%M-%S")}.log',
-                    filemode='w')
+					format='[%(asctime)s] %(levelname)s: %(message)s',
+					handlers=[logging.FileHandler(f'logs/{time.strftime("%Y-%m-%d_%H-%M-%S")}.log'),logging.StreamHandler()])
 
 def get_logger(name):
-    return logging.getLogger(name)
+	return logging.getLogger(name)
